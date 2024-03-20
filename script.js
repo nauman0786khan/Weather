3c0f802382bec4bd1d5c4d592e1cedc0fe41bb20
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-03-20 11:14:04 +0000

    doneweather

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ return vv.json();
 }).then((rr)=>{
 
 console.log(rr);
-let temp = `<h2>${rr.main.temp}</h2><p>feels like ${rr.main.feels_like}</p>`
+let temp = `<h2>${(rr.main.temp-273.15).toFixed(2)}</h2><p>feels like ${(rr.main.feels_like-273.15).toFixed(2)}</p>`
 let name = `<h2>${rr.name}</h2>`
 let humidity = `<h2>${rr.main.humidity}</h2>`
 let pressure = `<h2>${rr.main.pressure}</h2>`
-let wind = `<h2>deg ${rr.wind.deg} Speed ${rr.wind.speed}</h2>`
+let wind = `<h2>deg ${rr.wind.deg} Speed ${(rr.wind.speed*3.6).toFixed(2)}Km/h</h2>`
 let location =`<div style="max-width:100%;overflow:hidden;color:red;width:100%;height:500px;"><div id="google-maps-display" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=${rr.name},+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="the-googlemap-enabler" href="https://www.bootstrapskins.com/themes" id="authmaps-data">premium bootstrap themes</a><style>#google-maps-display img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style></div>`
 let wc = rr.weather[0].main;
 console.log(wc)
@@ -28,7 +28,14 @@ if(wc=="Clouds"){
 
     document.getElementById("vidc").src="Video/pexels_videos_1542008 (1080p).mp4";
 }
+else if(wc == "Haze"){
+    document.getElementById("vidc").src="Video/production_id_4687100 (2160p).mp4";
 
+}
+
+var tm = rr.main.temp
+var cal = (tm-273.15);
+console.log(cal)
 document.getElementById("gettemp").innerHTML=temp
 document.getElementById("getname").innerHTML=name
 document.getElementById("gethum").innerHTML=humidity
