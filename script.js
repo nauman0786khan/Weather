be1d6555751202dae667e77d14aec19828025711
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-04-10 09:40:10 +0000

    ui done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getweather(){
 
-let w = document.getElementById("scan").value;
+var w = document.getElementById("scan").value;
     console.log(w)
 
 let v = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${w}&appid=beea3435003dac5511b106b3cd2c6350`);
@@ -12,36 +12,33 @@ return vv.json();
 }).then((rr)=>{
 
 console.log(rr);
-let temp = `<h2>${(rr.main.temp-273.15).toFixed(2)}</h2><p>feels like ${(rr.main.feels_like-273.15).toFixed(2)}</p>`
+let temp = `<h2>${(rr.main.temp-273.15).toFixed(2)}&#176C</h2><p>Feels like ${(rr.main.feels_like-273.15).toFixed(2)}&#176C</p>`
 let name = `<h2>${rr.name}</h2>`
 let humidity = `<h2>${rr.main.humidity}</h2>`
 let pressure = `<h2>${rr.main.pressure}</h2>`
-let wind = `<h2>deg ${rr.wind.deg} Speed ${(rr.wind.speed*3.6).toFixed(2)}Km/h</h2>`
+let wind = `<h2>${(rr.wind.speed*3.6).toFixed(2)}<span style=font-size:16px;>Km/h</sp></h2>`
 let location =`<div style="max-width:100%;overflow:hidden;color:red;width:100%;height:500px;"><div id="google-maps-display" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=${rr.name},+India&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="the-googlemap-enabler" href="https://www.bootstrapskins.com/themes" id="authmaps-data">premium bootstrap themes</a><style>#google-maps-display img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style></div>`
 let wc = rr.weather[0].main;
 console.log(wc)
-if(wc=="Clouds"){
-    document.getElementById("vidc").src="Video/pexels_videos_3723 (1080p).mp4";
-
-
-} else if (wc=="Clear"){
-
-    document.getElementById("vidc").src="Video/pexels_videos_1542008 (1080p).mp4";
-}
-else if(wc == "Haze"){
-    document.getElementById("vidc").src="Video/production_id_4687100 (2160p).mp4";
-
-}
-
+let highlo=`<div id="High"><p>High ${(rr.main.temp_max-273.15).toFixed(2)}&#176C</p></div><div id="low"><p>Low ${(rr.main.temp_min-273.15).toFixed(2)}&#176C</p></div>`
+console.log(humidity);
+let locname=`<Button class="location btn">${w}</Button>`
+console.log(locname)
+let deg = `<h2>${rr.wind.deg}</h2>`
 var tm = rr.main.temp
 var cal = (tm-273.15);
-console.log(cal)
-document.getElementById("gettemp").innerHTML=temp
-document.getElementById("getname").innerHTML=name
-document.getElementById("gethum").innerHTML=humidity
-document.getElementById("getpre").innerHTML=pressure
-document.getElementById("getwind").innerHTML=wind
-document.getElementById("locget").innerHTML=location
+document.getElementById("getn").innerHTML= locname;
+document.getElementById("highlow").innerHTML=highlo;
+document.getElementById("gettemp").innerHTML=temp;
+document.getElementById("getname").innerHTML= name;
+document.getElementById("locget").innerHTML=location;
+document.getElementById("getwind").innerHTML=wind;
+document.getElementById("getpre").innerHTML=pressure;
+document.getElementById("deg").innerHTML=deg;
+
+document.getElementById("gethum").innerHTML= humidity;
+
+
 let wh = document.getElementById("bo");
 wh.classList.add("boxw1");
 
